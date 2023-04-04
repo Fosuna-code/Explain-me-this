dcd6eb4a30f558caa4384ffeefecd075f9ff4704
Fosuna-Code <ferosuna2001@gmail.com>
2023-04-04 12:11:42 -0700

    change presentational image size and confg tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-            "./src/**/*"],
+  content: ["./**/*.{html,js}"],
 
   presets: [],
   darkMode: 'media', // or 'class'
